ce9873713730d8692274c48387bba77b748d2029
Andrius Laukavičius <info@oerp.lt>
2014-07-30 14:44:50 +0300

    Created addresses archive model to store all addresses changes

--- crm_dc_extend/models/res_partner.py
+++ crm_dc_extend/models/res_partner.py
@@ -21,10 +21,24 @@
 ##############################################################################
 from openerp import models, fields
 
+class res_partner_address_archive(models.Model):
+    _name = 'res.partner.address_archive'
+    _description = 'Partner Addresses Archive'
+    #fields
+    country_id = fields.Many2one('res.country', 'Country')
+    state_id = fields.Many2one('res.country.state', 'State')
+    city = fields.Char('City')
+    name = fields.Char('Post Code', size=24)
+    street = fields.Char('Street')
+    current = fields.Boolean('Current')
+    partner_id = fields.Many2one('res.partner', 'Partner')
+
+    _sql_constraints = [('unique_current', 'unique(current)', 'Only One Address can be Current!')]
+
 class res_partner(models.Model):
     _name = 'res.partner'
     _inherit = 'res.partner'
-
     #fields
     nationality_id = fields.Many2one('res.country', 'Nationality')
     attention = fields.Text('Attention')
+    address_archive_ids = fields.One2many('res.partner.address_archive', 'partner_id', 'Addresses Archive')
