1cb3a10ecb45a80ca5339141ecfd8494612af60c
Andrius Laukavičius <info@oerp.lt>
2014-08-01 14:05:07 +0300

    Updated create_partner method with new fields

--- crm_location_extension/models/crm_lead.py
+++ crm_location_extension/models/crm_lead.py
@@ -20,7 +20,8 @@
 #
 ##############################################################################
 from openerp import models, fields
-from openerp.api import onchange
+from openerp.api import onchange, one
+from openerp import tools
 
 class crm_lead(models.Model):
     _name = 'crm.lead'
@@ -40,4 +41,31 @@ class crm_lead(models.Model):
             self.state_id = self.zip_id.state_id and self.zip_id.state_id.id or False
             self.house_no = self.zip_id.house_no or False
             self.apartment_no = self.zip_id.apartment_no or False
+
+    def _lead_create_contact(self, cr, uid, lead, name, is_company, parent_id=False, context=None):
+        partner = self.pool.get('res.partner')
+        vals = {'name': name,
+            'user_id': lead.user_id.id,
+            'comment': lead.description,
+            'section_id': lead.section_id.id or False,
+            'parent_id': parent_id,
+            'phone': lead.phone,
+            'mobile': lead.mobile,
+            'email': tools.email_split(lead.email_from) and tools.email_split(lead.email_from)[0] or False,
+            'fax': lead.fax,
+            'title': lead.title and lead.title.id or False,
+            'function': lead.function,
+            'street': lead.street,
+            'street2': lead.street2,
+            'zip': lead.zip,
+            'city': lead.city,
+            'country_id': lead.country_id and lead.country_id.id or False,
+            'state_id': lead.state_id and lead.state_id.id or False,
+            'is_company': is_company,
+            'type': 'contact',
+            'house_no': lead.house_no,
+            'apartment_no': lead.apartment_no,
+        }
+        partner = partner.create(cr, uid, vals, context=context)
+        return partner    
            
