f98e3af86a2169515b26e05b000cf952bac081fb
Andrius Laukavičius <info@oerp.lt>
2014-08-07 13:23:18 +0300

    init calendar_domestic_cleaning module

---
+++ calendar_domestic_cleaning/__openerp__.py
@@ -0,0 +1,53 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#    
+#    Odoo, Open Source Management Solution
+#
+#    Author: Andrius Laukavičius. Copyright: JSC NOD Baltic
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.     
+#
+##############################################################################
+
+
+{
+    'name': 'Calendar Domestic Cleaning',
+    'version': '1.0',
+    'category': 'Base',
+    'sequence': 2,
+    'summary': 'Calendar Domestic Cleaning',
+    'description': """
+	This module manages domestic cleaning specific events.
+	""",
+    'author': 'Andrius Laukavičius',
+    'website': 'OERP.EU',
+    'depends': [      
+    ],
+    'data': [
+        #'security/ir.model.access.csv',
+        #'views/,
+        #'data/,        
+
+    ],
+    'demo': [
+    ],
+    'test': [
+
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'images': [],
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
