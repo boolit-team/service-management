89f35ec78ff71f2e5054ec6d77471f3e4a0080ac
Eivydas Vilčinskas <apprentice@boolit.eu>
2016-01-05 17:05:21 +0200

    [UPG] base_location_extension: move to v8 API, fix 'city' requirement from parent module

--- base_location_extension/better_zip.py
+++ base_location_extension/better_zip.py
@@ -19,11 +19,16 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-#from openerp.osv import orm, fields
-from openerp import fields, models
+# from openerp.osv import orm, fields
+from openerp import api, fields, models
+
+# Import logger
+import logging
+# Get the logger
+_logger = logging.getLogger(__name__)
+
 
 class BetterZip(models.Model):
-
     _inherit = 'res.better.zip'
 
     city_id = fields.Many2one('res.country.state.city', 'City')
@@ -32,58 +37,91 @@ class BetterZip(models.Model):
     code = fields.Char('City Code', related='city_id.code', size=64, help="The official code for the city")
     house_no = fields.Char('House No.', size=32)
     apartment_no = fields.Char('Appartment No.', size=32)
-       
-    #_order = 'name asc'
-    def name_get(self, cursor, uid, ids, context=None):
-        res = []
-        for bzip in self.browse(cursor, uid, ids):
-            name = [bzip.name]
-            #Updating with street and city tables
-            if bzip.street_id:
-                name.append(bzip.street_id.name)
-            if bzip.city_id:
-                name.append(bzip.city_id.name)
-            if bzip.state_id:
-                name.append(bzip.state_id.name)
-            if bzip.country_id:
-                name.append(bzip.country_id.name)
-            res.append((bzip.id, ", ".join(name)))
-        return res
 
-    def onchange_city_id(self, cr, uid, ids, city_id=False, context=None):
+    # _order = 'name asc'
+    # def name_get(self, cursor, uid, ids, context=None):
+    #     res = []
+    #     for bzip in self.browse(cursor, uid, ids):
+    #         name = [bzip.name]
+    #         # Updating with street and city tables
+    #         if bzip.street_id:
+    #             name.append(bzip.street_id.name)
+    #         if bzip.city_id:
+    #             name.append(bzip.city_id.name)
+    #         if bzip.state_id:
+    #             name.append(bzip.state_id.name)
+    #         if bzip.country_id:
+    #             name.append(bzip.country_id.name)
+    #         res.append((bzip.id, ", ".join(name)))
+    #     return res
 
-        if city_id:
-            city = self.pool['res.country.state.city'].browse(cr, uid, city_id, context=context)
-            return {'value': {'state_id': city.state_id.id if city.state_id else False,
-                          'code': city.code,
-                          }
-                 }
-        else:
-            return {}
+    # @api.one
+    # @api.depends(
+    #     'name',
+    #     'street_id',
+    #     'city_id',
+    #     'state_id',
+    #     'country_id',
+    # )
+    # def _get_display_name(self):
+    #     if self.name:
+    #         name = [self.name, self.city_id]
+    #     else:
+    #         name = [self.city_id]
+    #     if self.street_id:
+    #         name.append(self.city_id)
+    #     if self.city_id:
+    #         name.append(self.city_id)
+    #     if self.state_id:
+    #         name.append(self.state_id)
+    #     if self.country_id:
+    #         name.append(self.country_id)
+    #     self.display_name = ", ".join(name)
 
-    def onchange_street_id(self, cr, uid, ids, street_id=False, context=None):
+    @api.onchange('city_id')
+    # def onchange_city_id(self, cr, uid, ids, city_id=False, context=None):
+    def onchange_city_id(self):
+        # _logger.warning(self)
+        if self.city_id:
+            # _logger.critical(str(self.city_id))
+            # _logger.critical(str(self.city_id.id))
+            # _logger.critical(str(self.city_id.name))
+            # self.city = self.env['res.country.state.city'].browse(self.city_id.id)
+            # _logger.warning(str(self.city))
+            # _logger.critical(str(self.city_id.state_id.id))
+            # _logger.critical(str(self.city_id.state_id.name))
 
-        if street_id:
-            street = self.pool['res.country.state.city.street'].browse(cr, uid, street_id, context=context)
-            return {'value': {'city_id': street.city_id.id if street.city_id else False,
-                          'code': street.city_id.code,
-                          }
-                 }
-        else:
-            return {}
+            self.state_id = self.city_id.state_id.id if self.city_id.state_id else False
+            self.code = self.city_id.code
+            # return {'value': {'state_id': self.city.state_id.id if self.city.state_id else False,
+            #                   'code': self.city.code,
+            #                   }
+            #         }
 
-    def name_search(self, cr, uid, name, args=None, operator='ilike', context=None, limit=5):
-        if args is None:
-            args = []
-        if context is None:
-            context = {}
-        ids = []
-        if name:
-            ids = self.search(cr, uid, [('name', operator, name)] + args, limit=limit)
-        if not ids:
-            ids = self.search(cr, uid, [('city_id', operator, name)] + args, limit=limit)
-        if not ids:
-            ids = self.search(cr, uid, [('street_id', operator, name)] + args, limit=limit)       
-        return self.name_get(cr, uid, ids, context=context)                                          
-            
-       
+    @api.onchange('street_id')
+    # def onchange_street_id(self, cr, uid, ids, street_id=False, context=None):
+    def onchange_street_id(self):
+
+        if self.street_id:
+            # _logger.critical("this is SPARTAAA!!!" + str(self.street_id))
+            # self.street = self.env['res.country.state.city.street'].browse(self.street_id).id
+            self.city_id = self.street_id.city_id.id if self.street_id.city_id else False
+            self.code = self.street_id.city_id.code
+            # return {'value': {'city_id': street.city_id.id if street.city_id else False,
+            #                   'code': street.city_id.code,
+            #                   }
+            #         }
+
+    # def name_search(self, cr, uid, name, args=None, operator='ilike', context=None, limit=5):
+    #     if args is None:
+    #         args = []
+    #     if context is None:
+    #         context = {}
+    #     ids = []
+    #     if name:
+    #         ids = self.search(cr, uid, [('name', operator, name)] + args, limit=limit)
+    #     if not ids:
+    #         ids = self.search(cr, uid, [('city_id', operator, name)] + args, limit=limit)
+    #     if not ids:
+    #         ids = self.search(cr, uid, [('street_id', operator, name)] + args, limit=limit)
+    #     return self.name_get(cr, uid, ids, context=context)
